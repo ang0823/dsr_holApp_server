5d27ef666c49a74ec9b47cfa05122f98a0a1ad4e
EnriqueCeballosMartinez <ceballos_kik96@hotmail.com>
2019-11-19 19:13:30 -0600

    Se agregaron archivos GRPC

---
+++ gRPC_Server/server.py
@@ -0,0 +1,12 @@
+import grpc
+import ProtocolBuffer_pb2_grpc
+import ProtocolBuffer_pb2 
+from concurrent import futures
+
+class ServidorChat (ProtocolBuffer_pb2_grpc)
+class ServidorChat (defFunciones.MessagesServicer):
+
+    def __init__(self):
+        self.iniciado = True
+
+    def coversar:
